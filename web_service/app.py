bab403760109c8463b73305e9d439f2e790f7dd8
Roman <Cherkasov.R.V@sberbank.ru>
2021-08-21 21:03:31 +0300

    streamlit: multiple assignments

--- web_service/app.py
+++ web_service/app.py
@@ -4,11 +4,7 @@
 """
 
 import streamlit as st
-# To make things easier later, we're also importing numpy and pandas for
-# working with sample data.
-# import numpy as np
 import pandas as pd
-# import os
 import subprocess as sp
 import shlex
 import json
@@ -28,19 +24,15 @@ if uploaded_files is not None:
         )
         output = normal.stdout
         obj = json.loads(output)
-        # data = list()
         left, right = st.columns(2)
         with left:
-            # st.header("Текст")
             st.markdown("<h2 style='text-align: center;'>Текст</h2>", unsafe_allow_html=True)
             st.write(obj["text"])
         with right:
-            # st.header("   Поручения")
             st.markdown("<h2 style='text-align: center;'>Поручения</h2>", unsafe_allow_html=True)
             st.markdown("---")
         for record in obj["cards"]:
             with right:
-                # st.header(record['responsible']['text'])
                 st.write(record["assignment"]["text"])
                 st.caption(f"Ответственный: {record['responsible']['text']}")
                 st.caption(f"Срок: {record['date']['text']}")
